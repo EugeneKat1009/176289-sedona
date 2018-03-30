e58b7aa66ba4257958b0e8839bb32604ceec682b
EugeneKat <zhenkatusha@gmail.com>
2018-03-30 13:21:27 +0300

    сборка

--- js/popup.js
+++ js/popup.js
@@ -11,17 +11,14 @@ if (!popup.classList.contains("animation")) {
 }
 popup.classList.toggle("modal-hide");
 arrivalDate.focus();
-popup.classList.remove("js-booking-item");
 popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
 evt.preventDefault();
 if(!arrivalDate.value && !leavingDate.value) {
-  popup.classList.remove("js-booking-item");
   popup.classList.remove("modal-error");
   form.offsetWidth = form.offsetWidth;
-  popup.classList.add("js-booking-item");
   popup.classList.add("modal-error");
 }
 });
