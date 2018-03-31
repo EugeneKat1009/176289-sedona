739b51c6a3e46b23d4e524d21b2e59546443ada8
EugeneKat1009 <zhenkatusha@gmail.com>
2018-03-31 17:01:11 +0300

    внес правки

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,24 @@
-var link = document.querySelector(".search-btn");
-var popup = document.querySelector(".modal");
+var button = document.querySelector(".search-btn");
+var popup = document.querySelector(".booking-form");
 var arrivalDate = popup.querySelector("[name=arrival]");
 var leavingDate = popup.querySelector("[name=leaving]");
-var form = popup.querySelector("form");
+var form = document.querySelector("form");
 
-link.addEventListener("click", function (evt) {
-evt.preventDefault();
-if (!popup.classList.contains("animation")) {
-  popup.classList.add("animation")
-}
-popup.classList.toggle("modal-hide");
-arrivalDate.focus();
-popup.classList.remove("modal-error");
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  if (!popup.classList.contains("animation")) {
+    popup.classList.add("animation")
+  }
+  popup.classList.toggle("modal-hide");
+  arrivalDate.focus();
+  popup.classList.remove("modal-error");
 });
 
 form.addEventListener("submit", function(evt) {
-evt.preventDefault();
-if(!arrivalDate.value && !leavingDate.value) {
-  popup.classList.remove("modal-error");
-  form.offsetWidth = form.offsetWidth;
-  popup.classList.add("modal-error");
-}
+  evt.preventDefault();
+  if (!arrivalDate.value && !leavingDate.value) {
+    popup.classList.remove("modal-error");
+    form.offsetWidth = form.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
